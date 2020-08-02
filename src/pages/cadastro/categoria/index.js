1728546a99c2ab01deb5b7bb34977a5ceb15d797
Allan Gregory <alngregory@gmail.com>
2020-08-02 18:10:05 -0300

    Aula 04 - Commit para subir o front-end que possui o arquivo db.json e tela de cadastro de categoria com css arrumado.

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,7 +1,8 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect }  from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/pageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/button';
 
 function CadastroCategoria() {
 
@@ -28,6 +29,37 @@ function CadastroCategoria() {
     );
   }
 
+  useEffect(() => {
+
+    const Url = 'http://localhost:8080/categorias';
+
+    fetch(Url)
+      .then(async (respostaServidor) => {
+        const resposta = await respostaServidor.json();
+        setCategorias([
+          ...resposta
+        ]);
+      });
+
+    // setTimeout(() => {
+    //   setCategorias([
+    //     ...categorias,
+    //     {
+    //       id: 1,
+    //       nome: 'Front-End',
+    //       descricao: 'Uma categoria front-end',
+    //       cor: '#cbd1ff'
+    //     },
+    //     {
+    //       id: 2,
+    //       nome: 'Back-End',
+    //       descricao: 'Uma categoria back-end',
+    //       cor: '#cbd1ff'
+    //     }
+    //   ]);
+    // }, 4 * 1000);
+  }, []);
+
     return (
       <PageDefault>
         <h1>Cadastro de Categoria: {values.nome}</h1>
@@ -53,7 +85,7 @@ function CadastroCategoria() {
 
           <FormField
             label="Descrição"
-            type="????"
+            type="textarea"
             name="descricao"
             value={values.descricao}
             onChange={handleChange}
@@ -67,15 +99,21 @@ function CadastroCategoria() {
             onChange={handleChange}
           />
           
-          <button>
+          <Button>
             Cadastrar
-          </button>
+          </Button>
         </form>
 
+        {categorias.length === 0 && (
+          <div>
+            Carregando...
+          </div>
+        )}
+
         <ul>
-          {categorias.map((categoria, indice) => {
+          {categorias.map((categoria) => {
             return (
-              <li key={`${categoria}${indice}`}>
+              <li key={`${categoria.nome}`}>
                 {categoria.nome}
               </li>
             )
